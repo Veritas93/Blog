9a105344df13fc26a088916a909a459d31558096
сергей костин <loli93@yandex.ru>
2022-01-30 15:04:49 +0300

    Disolay commets for post

--- app.rb
+++ app.rb
@@ -69,7 +69,7 @@ get '/details/:post_id' do
 	 # выбираем этот пост в переменную @row
 	@row = results[0]
 	 # выбираем комментарии для нашего поста 
-	 @comments = db.execute 'select * from Comments where post_id = ? order by id', [post_id]
+	 @comments = @db.execute 'select * from Comments where post_id = ? order by id', [post_id]
 
 	erb :details
 end
