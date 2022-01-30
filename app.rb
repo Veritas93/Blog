9f7e0b8758c695610312efb9d8abf873a8815016
сергей костин <loli93@yandex.ru>
2022-01-30 14:47:30 +0300

    Save comment to database

--- app.rb
+++ app.rb
@@ -79,5 +79,20 @@ post '/details/:post_id' do
 
 	# получаем переменную из post запроса 
 	content = params[:content]
-	erb "You type comment #{content} for post #{post_id}"
+
+	 # сохранение данных в БД
+    @db.execute 'insert into Comments
+     (
+     	content,
+     	created_date,
+    	post_id
+    ) 
+    	values 
+    ( 
+    	? ,
+    	datetime(),
+    	?
+    )' , [content, post_id]
+
+	redirect to ('/details/' + post_id)
 end
